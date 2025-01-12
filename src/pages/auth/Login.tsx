69f4dbf4f93ebcbd3fb5e26e63b656900ec4f270
nitinroyal12 <radheradhe9870023@gmail.com>
2025-01-12 18:45:57 +0530

    project setup

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactElement } from 'react';
+
+const Login:FC = ():ReactElement => {
+  return (
+    <div>Login</div>
+  );
+};
+
+export default Login;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsConfigPath from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tsConfigPath()],
+  resolve:{
+    alias:{
+      "@":"src/",
+      "@pages":"src/pages/",
+      "@components":"src/components/",
+      "@store":"src/store/"
+    }
+  }
+});
